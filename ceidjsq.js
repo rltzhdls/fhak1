6887460c11b1e12d3ea0975799fcd5a57aa67736
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-11-07 14:21:18 +0900

    Add files via upload

---
+++ ceidjsq.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/Lineage2M/l2m_0812_main_bg.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
